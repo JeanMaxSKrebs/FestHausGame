8101e04a8b63a3ae15d192bdddc4c3e79bb3adcd
JeanMaxSKrebs <jeanmaxskrebs@gmail.com>
2026-05-14 11:49:34 -0300

    fix: ajusta rotas e configuracoes android

--- app/screens/_layout.tsx
+++ app/screens/_layout.tsx
@@ -3,10 +3,10 @@ import { Stack } from 'expo-router';
 export default function ScreensLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="SignIn/index" />
-      <Stack.Screen name="SignUp/index" />
-      <Stack.Screen name="Home/index" />
-      <Stack.Screen name="Game/index" />
+      <Stack.Screen name="SignIn" />
+      <Stack.Screen name="SignUp" />
+      <Stack.Screen name="Home" />
+      <Stack.Screen name="Game" />
     </Stack>
   );
 }
